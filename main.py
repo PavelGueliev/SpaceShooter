9480feff106ab54bd6eeea6fd223eebe291ab275
PavelGuliev <pavel12345g@gmail.com>
2022-01-08 15:40:30 +0300

    Добавили картинки кнопок и сделали очки со здоровьем

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ vertical_borders = pygame.sprite.Group()
 stars_sprites = pygame.sprite.Group()
 menu_sprites = pygame.sprite.Group()
 pygame.init()
-size = width, height = 500, 700
+size = width, height = 1280, 720
+running = True
 v = 50
 fps = 60
 tic = 10
+score = 0
 buf_of_level = []
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Space shooter')
 count = 0
 
 
@@ -40,9 +43,23 @@ class Hero(pygame.sprite.Sprite):
         self.swim = self.y + 10
         self.flag_swim = 1
         self.effect_bullet = 2
+        self.hp = 30
 
     def update(self, fly):
         self.rect = pygame.Rect(self.x, self.y, self.size_square, self.size_square)
+        if tic % 15 == 0:
+            if pygame.sprite.spritecollideany(self, Enemy_sprites):
+                self.hp -= 1
+                print('hp', self.hp)
+            if pygame.sprite.spritecollideany(self, Enemy_sprites_2):
+                self.hp -= 1
+                print('hp', self.hp)
+        if pygame.sprite.spritecollideany(self, enemy_bullet_sprites):
+            self.hp -= 1
+            print('hp', self.hp)
+
+    def get_hp(self):
+        return self.hp
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -109,6 +126,9 @@ class Bullet_of_Enemy(pygame.sprite.Sprite):
         self.x, self.y = pos[0], pos[1]
 
     def update(self):
+        if pygame.sprite.spritecollideany(self, hero_sprites):
+            self.kill()
+            return
         self.rect = pygame.Rect(self.x, int(self.y), self.just_size[0], self.just_size[1])
         self.y += 10
 
@@ -158,17 +178,21 @@ class Enemy_type_1(pygame.sprite.Sprite):
         pygame.draw.rect(self.image, pygame.Color("pink"), (0, 0, self.size_of_enemy[0], self.size_of_enemy[1]))
         self.rect = pygame.Rect(*pos, *self.size_of_enemy)
         self.x, self.y = pos
+        self.ux = 30
+        self.uy = 30
 
     def update(self):
+        global score
         if pygame.sprite.spritecollideany(self, bullets_sprites):
+            score += 25
             self.kill()
             return
         if tic % 2 == 0:
-            self.rect = self.rect.move(self.x * 0.5, self.y * 0.15)
+            self.rect = self.rect.move(self.ux * 0.5, self.uy * 0.15)
             if pygame.sprite.spritecollideany(self, horizontal_borders):
-                self.y = -self.y
+                self.uy = -self.uy
             if pygame.sprite.spritecollideany(self, vertical_borders):
-                self.x = -self.x
+                self.ux = -self.ux
 
 
 class Enemy_type_2(pygame.sprite.Sprite):
@@ -182,18 +206,22 @@ class Enemy_type_2(pygame.sprite.Sprite):
         pygame.draw.rect(self.image, pygame.Color("#DC143C"), (0, 0, self.size_of_enemy[0], self.size_of_enemy[1]))
         self.rect = pygame.Rect(*pos, self.size_of_enemy[0], self.size_of_enemy[1])
         self.x, self.y = pos
+        self.ux = 15
+        self.uy = 1
 
     def update(self):
+        global score
         if tic % 15 == 0:
             Bullet_of_Enemy((self.rect.x, self.rect.y + 10))
 
         if pygame.sprite.spritecollideany(self, bullets_sprites):
             self.kill()
+            score += 50
             return
 
-        self.rect = self.rect.move(self.x * 0.15, self.y * 0)
+        self.rect = self.rect.move(self.ux * 0.15, self.uy)
         if pygame.sprite.spritecollideany(self, vertical_borders):
-            self.x = -self.x
+            self.ux = -self.ux
 
 
 class Enemy_type_3(pygame.sprite.Sprite):
@@ -210,9 +238,11 @@ class Enemy_type_3(pygame.sprite.Sprite):
         self.x, self.y = pos
 
     def update(self):
+        global score
         if pygame.sprite.spritecollideany(self, bullets_sprites):
             self.count += 1
             if self.count == 150:
+                score += 500
                 self.kill()
                 return
 
@@ -252,14 +282,14 @@ def start_screen(command='continue'):
     start_img = pygame.image.load('Game.png').convert_alpha()
     exit_img = pygame.image.load('Exit.png').convert_alpha()
     credit_img = pygame.image.load('Credit.png').convert_alpha()
-    us_img = pygame.image.load('Us.png').convert_alpha()
     reit_img = pygame.image.load('Reit.png').convert_alpha()
+    splashscreen_img = pygame.image.load('SplashScreen.png').convert_alpha()
     # create button instances
-    start_button = Button(100, 200, start_img, 0.8)
-    exit_button = Button(100, 250, exit_img, 0.8)
-    credit_button = Button(100, 300, credit_img, 0.8)
-    reit_button = Button(100, 350, reit_img, 0.8)
-    us_button = Button(100, 400, us_img, 0.8)
+    start_button = Button(width * 0.4, height * 0.4, start_img, 1.5)
+    reit_button = Button(width * 0.4, height * 0.5, reit_img, 1.5)
+    credit_button = Button(width * 0.4, height * 0.6, credit_img, 1.5)
+    exit_button = Button(width * 0.4, height * 0.7, exit_img, 1.5)
+    Button(width * 0.26, height * 0.13, splashscreen_img, 2)
 
     # game loop
     if command == 'start':
@@ -272,10 +302,17 @@ def start_screen(command='continue'):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                terminate()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE and command != 'start':
                     return
         if start_button.draw(screen):
+            start_time = datetime.now()
+            return
+        if reit_button.draw(screen):
+            start_time = datetime.now()
+            return
+        if credit_button.draw(screen):
             start_time = datetime.now()
             return
         if exit_button.draw(screen):
@@ -320,12 +357,13 @@ def load_level(txt_file):
 
 
 def main():
-    global tic, start_time, buf_of_level
+    global tic, start_time
     buf_of_level = load_level('1.txt')
     start_screen('start')
     clock = pygame.time.Clock()
     running = True
     Hero((int(width * 0.5), int(height * 0.75)))
+
     directions = {"right": False, "left": False, 'mouse': False, 'down': False, 'up': False}
     Border(5, 5, width - 5, 5)
     Border(5, height - 5, width - 5, height - 5)
@@ -385,7 +423,7 @@ def main():
                     spr.y -= 10
         if directions['down']:
             for spr in hero_sprites:
-                if spr.y < height - 20:
+                if spr.y < height - 25:
                     spr.y += 10
         if directions['mouse']:
             if tic % 15 == 0:
@@ -410,11 +448,9 @@ def main():
             tic = 0
         tic += 1
         try:
-            print(int((datetime.now() - start_time).total_seconds() * 100) % 100)
             if int((datetime.now() - start_time).total_seconds()) >= buf_of_level[0][0] and\
                     0 <= int((datetime.now() - start_time).total_seconds() * 100) % 100 < 2:
                 for i in range(buf_of_level[0][2]):
-                    print(buf_of_level[0][3][i])
                     if buf_of_level[0][1] == 1:
                         Enemy_type_1(buf_of_level[0][3][i])
                     elif buf_of_level[0][1] == 2:
@@ -425,6 +461,13 @@ def main():
                 del buf_of_level[0]
         except:
             pass
+
+        font = pygame.font.SysFont('serif', 24)
+        img = font.render(f'Score: {score}', True, 'green')
+        for i in hero_sprites:
+            img2 = font.render('HP:' + ''.join(['|' for i in range(i.get_hp())]), True, 'green')
+
+
         stars_sprites.draw(screen)
         hero_sprites.draw(screen)
         Enemy_sprites.draw(screen)
@@ -440,6 +483,8 @@ def main():
         hero_sprites.update(directions['down'] or directions['up'])
         Enemy_sprites.update()
 
+        screen.blit(img, (width * 0.9, height * 0.1))
+        screen.blit(img2, (width * 0.1, height * 0.1))
         pygame.display.flip()
         clock.tick(fps)
 
